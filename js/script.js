29482d97b41f982d22f930d219e8eb5cde241763
joechan3 <joechan3@gmail.com>
2016-04-24 21:20:54 -0600

    Update json with modified dataset from thewebminer.com; developjs

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-//Test JSON hosted here: https://api.myjson.com/bins/3pqdy
+//Test JSON hosted here: https://api.myjson.com/bins/2xkni
 //Same domain location: json/joeQuotes.json
+//Original quotes dataset courtesy of http://thewebminer.com/download
 
 $("document").ready(function() {
   function getNewQuote() {
-    $.getJSON("https://api.myjson.com/bins/3pqdy", function(data) {
+    $.getJSON("https://api.myjson.com/bins/2xkni", function(data) {
 
       //Pick a random whole number between 0 (inclusive) and 
       //the number of quotes available (exclusive)
       var randomNumber = Math.floor(Math.random() * ((data.quoteCollection.length - 1) - 0 + 1)) + 0;
 
-      //Placeholder: Update HTML for quote and author
-
+      //Update HTML for quote and author
+      $("article>blockquote p").text(data.quoteCollection[randomNumber].quote);
+      $("article cite").text(data.quoteCollection[randomNumber].author);
     });
   }
-
-  //Placeholder: click event function
+  
+  //Display initial quote when page loads
   getNewQuote();
+  
+  //Display new quote when New Joseph Quote button is clicked
+  $("button:first-of-type").click(function(){
+    getNewQuote(); 
+  });
 
 });
