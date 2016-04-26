0021fe513b679d3ae42aa5ee9f341e7e87119575
joechan3 <joechan3@gmail.com>
2016-04-25 19:33:55 -0600

    Fix basic functionality of ajax request to Random.org

--- js/script.js
+++ js/script.js
@@ -2,26 +2,36 @@
 //Same domain location: json/joeQuotes.json
 //Original quotes dataset by The Web Miner (http://thewebminer.com/download)
 //True random number serivce by Random.org (to implement)
+//Thank you: http://stackoverflow.com/questions/8147211/jquery-jsonrpc-2-0-call-via-ajax-gets-correct-response-but-does-not-work
 
 $("document").ready(function() {
   function getNewQuote() {
     $.getJSON("https://api.myjson.com/bins/4yntq", function(data) {
 
-      //Get true random numbers
+      //Get true random numbers from Random.org
       $.ajax({
         url: "https://api.random.org/json-rpc/1/invoke",
         method: "POST",
-        contentType: "application/json-rpc"
-        data: {
+        contentType: "application/json-rpc",
+        dataType: "json",
+        data: JSON.stringify({
           jsonrpc: "2.0", 
           method: "generateIntegers", 
           params: 
           {"apiKey": "89f7b8ea-50ae-4cb0-ab6e-738ece846bbb",
-           "n": 100,
+           "n": 10,
            "min": 0,
            "max": data.quoteCollection.length-1},
-          id: 42}
-      });
+          id: 42})
+      })
+        .done(function(trueRandomNumbers){
+          //var foo = JSON.parse(trueRandomNumbers);
+          alert("Success!");
+          console.log(trueRandomNumbers.result.random.data[5]);
+    })
+        .fail(function(){
+          alert("fail");
+    });
       
       //Pick a random whole number between 0 (inclusive) and 
       //the number of quotes available (exclusive)
