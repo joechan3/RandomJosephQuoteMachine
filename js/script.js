4107b2c748becd877c61255f98f731b768448219
Joseph Chan <joechan3@gmail.com>
2016-04-26 21:37:43 -0600

    Add styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Test JSON hosted here: https://api.myjson.com/bins/1r49a
 //Same domain location: json/joeQuotes.json
-//Original quotes dataset by The Web Miner (http://thewebminer.com/download)
+//Thank you:
+  //Original quotes dataset by The Web Miner (http://thewebminer.com/download)
+  //Animate.css: https://github.com/daneden/animate.css
 
 $("document").ready(function() {
   "use strict";
@@ -13,13 +15,27 @@ $("document").ready(function() {
     var randomNumber = Math.floor(Math.random() * jsonQuotes.quoteCollection.length);
 
     //Update HTML for quote and author
-    $("article>blockquote p").text(jsonQuotes.quoteCollection[randomNumber].quote);
+    $("blockquote p").text(jsonQuotes.quoteCollection[randomNumber].quote);
     $("article cite").text(jsonQuotes.quoteCollection[randomNumber].author);
-
+    
+    
+    //Animation for quotes (from https://github.com/daneden/animate.css)
+    $.fn.extend({
+      animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+      }
+    });
+    $("blockquote").animateCss("pulse");
+    
     //Update Twitter button    
     var tweetString = "https://twitter.com/intent/tweet?text=";
     tweetString += ('"' + jsonQuotes.quoteCollection[randomNumber].quote + '" \u2014 ' + jsonQuotes.quoteCollection[randomNumber].author);
     $(".twitter-share-button").attr("href", tweetString);
+    
+    
   }
 
   //Get collection of quotes
