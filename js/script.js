6d93546c06b699ab147b6477e05f2689d32fe14a
Joseph Chan <joechan3@gmail.com>
2016-06-12 12:06:13 -0600

    JSLint

--- js/script.js
+++ js/script.js
@@ -2,45 +2,48 @@
 //   Original quotes dataset by The Web Miner (http://thewebminer.com/download)
 //   Animate.css: https://github.com/daneden/animate.css
 
-$("document").ready(function() {
-  "use strict";
+//jslint browser:true, this:true, single:true
+//global $
 
-  var jsonQuotes = new Object();
+$('document').ready(function () {
+    'use strict';
 
-  function generateQuote() {
-    //Pick a random whole number between 0 (inclusive) and 
-    //the number of quotes available (exclusive)
-    var randomNumber = Math.floor(Math.random() * jsonQuotes.quoteCollection.length);
+    var jsonQuotes = {};
 
-    //Update HTML for quote and author
-    $("blockquote p").text(jsonQuotes.quoteCollection[randomNumber].quote);
-    $("article cite").text(jsonQuotes.quoteCollection[randomNumber].author);
+    function generateQuote() {
+        //Pick a random whole number between 0 (inclusive) and
+        //the number of quotes available (exclusive)
+        var randomNumber = Math.floor(Math.random() * jsonQuotes.quoteCollection.length);
 
-    //Animation for quotes (from https://github.com/daneden/animate.css)
-    $.fn.extend({
-      animateCss: function(animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-          $(this).removeClass('animated ' + animationName);
+        //Update HTML for quote and author
+        $('blockquote p').text(jsonQuotes.quoteCollection[randomNumber].quote);
+        $('article cite').text(jsonQuotes.quoteCollection[randomNumber].author);
+
+        //Animation for quotes (from https://github.com/daneden/animate.css)
+        $.fn.extend({
+            animateCss: function (animationName) {
+                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+                $(this).addClass('animated ' + animationName).one(animationEnd, function () {
+                    $(this).removeClass('animated ' + animationName);
+                });
+            }
         });
-      }
+        $('blockquote').animateCss('pulse');
+
+        //Update Twitter button
+        var tweetString = 'https://twitter.com/intent/tweet?text=';
+        tweetString += ('"' + jsonQuotes.quoteCollection[randomNumber].quote + '"' + ' - ' + jsonQuotes.quoteCollection[randomNumber].author);
+        $('.twitter-share-button').attr('href', tweetString);
+    }
+
+    //Get collection of quotes
+    $.getJSON('json/joeQuotes.json', function (data) {
+        jsonQuotes = data;
+        generateQuote();
     });
-    $("blockquote").animateCss("pulse");
 
-    //Update Twitter button    
-    var tweetString = "https://twitter.com/intent/tweet?text=";
-    tweetString += ('"' + jsonQuotes.quoteCollection[randomNumber].quote + '"' + ' - ' + jsonQuotes.quoteCollection[randomNumber].author);
-    $(".twitter-share-button").attr("href", tweetString);
-  }
-
-  //Get collection of quotes
-  $.getJSON("json/joeQuotes.json", function(data) {
-    jsonQuotes = data;
-    generateQuote();
-  });
-
-  $("button").click(function() {
-    generateQuote();
-  });
+    $('button').click(function () {
+        generateQuote();
+    });
 
 });
